3ef5fd782bacb657ba856165f6323da77614593d
Emmanuel21300681 <barajastuzo@gmail.com>
2026-03-30 14:08:17 -0600

    BASE

---
+++ proyecto.c
@@ -0,0 +1,22 @@
+//Edgar Osvaldo Angel Mercado 
+//Emmanuel Barajas Arroyo 
+
+
+#include <stdio.h>
+#include <math.h>
+
+int generarNumero();
+unsigned char consumirEnergia(unsigned char energia);
+void mostrarEnergia(unsigned char energia);
+int longitudNumero(int n);
+void mostrarPistas(int secreto, int intento);
+void mostrarReglas();
+
+int main(){
+
+
+    return 0;
+}
+
+
+
