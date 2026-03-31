31b74adffc1776349eb0b904f3cd594442482ae6
Emmanuel21300681 <barajastuzo@gmail.com>
2026-03-30 18:53:31 -0600

    Final

--- proyecto.c
+++ proyecto.c
@@ -9,13 +9,14 @@
 
 #define MAX_INTENTOS 8
 
-// funciones persona 1
+
 int generarNumero();
 int longitudNumero(int n);
 int calcularSumaDigitos(int n);
 void mostrarPistas(int secreto, int intento);
 
-// funciones persona 2
+
+
 unsigned char consumirEnergia(unsigned char energia);
 void mostrarEnergia(unsigned char energia);
 void mostrarReglas();
@@ -43,17 +44,20 @@ int main()
         switch(opcion)
         {
 
-            case 1: // INICIAR JUEGO
+            case 1: 
             {
                 int secreto;
                 int intento;
                 int intentos = 0;
 
-                unsigned char energia = 255; // 11111111
+                unsigned char energia = 255; 
 
                 secreto = generarNumero();
 
                 printf("\nSe ha generado un numero secreto entre 1 y 99999\n");
+                printf("Energia: ");
+                mostrarEnergia(energia);
+                printf("\n");
 
                 while(intentos < MAX_INTENTOS && energia > 0)
                 {
@@ -73,6 +77,7 @@ int main()
                     {
                         printf("\n¡Felicidades! Adivinaste el numero.\n");
                         printf("El numero secreto era: %d\n", secreto);
+                        printf("Intentos usados: %d\n", intentos);
 
                         ganadas++;
                         break;
@@ -99,42 +104,40 @@ int main()
                     }
                 }
 
-                printf("\nPresiona una tecla para continuar...\n");
-                getchar();
-                getchar();
+                system("pause");
 
                 break;
             }
 
 
-            case 2: // REGLAS
+            case 2: 
                 mostrarReglas();
+                system("pause");
                 break;
 
 
-            case 3: // GRAFICO
+            case 3: 
             {
                 int total = ganadas + perdidas;
 
                 printf("\n----REPORTE DE PARTIDAS---\n");
 
                 printf("Ganadas: %d ", ganadas);
-                for(int i = 0; i < ganadas; i++)
+                int i;
+                for(i = 0; i < ganadas; i++)
                     printf("*");
 
                 printf("\nPerdidas: %d ", perdidas);
-                for(int i = 0; i < perdidas; i++)
+                for(i = 0; i < perdidas; i++)
                     printf("*");
 
                 printf("\nTotales: %d ", total);
-                for(int i = 0; i < total; i++)
+                for(i = 0; i < total; i++)
                     printf("*");
 
                 printf("\n");
 
-                printf("\nPresiona una tecla para continuar...\n");
-                getchar();
-                getchar();
+                system("pause");
 
                 break;
             }
@@ -154,7 +157,7 @@ int main()
     return 0;
 }
 
-//Funciones 1
+
 //Función que genera un número aleatorio entre 1 y 99999
 int generarNumero(){
     return (rand() % 99999) + 1;
@@ -189,9 +192,10 @@ Pista: El número es par.
 Pista: Divisible entre 7.
 */
 void mostrarPistas(int secreto, int intento){
-    int espar = 0
-    ultimo_digito = secreto % 10;
-    longitud = longitudNumero(secreto)
+    int espar = 0;
+    int ultimo_digito = secreto % 10;
+    int longitud = longitudNumero(secreto);
+    int suma = calcularSumaDigitos(secreto);
     if(secreto % 2 == 0){
         espar = 1;
     }else{
@@ -201,6 +205,7 @@ void mostrarPistas(int secreto, int intento){
     printf("Pista: El ultimo digito: %d\n", ultimo_digito);
     printf("Pista: Tiene %d cifras.\n", longitud);
     printf("Pista: El numero es %s.\n", (espar == 1) ? "par" : "impar");
+    printf("Pista: La suma de sus digitos es %d\n", suma);
 
     //Pruebas de divisibilidad entre 3, 5, 7
     if(secreto % 3 == 0){
@@ -218,5 +223,82 @@ void mostrarPistas(int secreto, int intento){
     }else{
         printf("El numero secreto es MENOR que tu intento\n");
     }
+
+}
+
+//quita un 1 y pone un 0   11111111  -> 01111111
+unsigned char consumirEnergia(unsigned char energia){
+
+    energia = energia >>1;
+    return energia;
+
+}
+
+// entero a binario   00001101  >> 7
+                    //00000000 & 1 = 0  bit = 0
+                    //00001101  >> 6
+                    //00000000 & 1 = 0  bit = 0
+                    //00001101  >> 5
+                    //00000000 & 1 = 0  bit = 0
+                    //00001101  >> 4
+                    //00000000 & 1 = 0  bit = 0
+                    //00001101  >> 3
+                    //00000001 & 1 = 0  bit = 1
+                    //00001101  >> 2
+                    //00000011 & 1 = 0  bit = 1
+                    //00001101  >> 1
+                    //00000110 & 1 = 0  bit = 0
+                    //00001101  >> 0 
+                    //00001101 & 1 = 0  bit = 1
+void mostrarEnergia(unsigned char energia){
+
+    int i;
+
+    for (i=7;i>=0;i--){
+        int bit = (energia>>i) &1;
+        printf("%d", bit);
+    }
+
+
+}
+
+void mostrarReglas(){
+
+    printf("_______________________________________________________\n");
+    printf("1. OBJETIVO\n");
+    printf("-------------------------------------------------------\n");
+    printf("El programa genera un numero secreto entre 1 y 99999.\nTu objetivo es adivinarlo.\n");
+    printf("_______________________________________________________\n");
+    printf("2. INTENTOS\n");
+    printf("-------------------------------------------------------\n");
+    printf("Tienes como maximo 8 intentos.\n");
+    printf("_______________________________________________________\n");
+    printf("3. ¿COMO FUNCIONA LA ENERGIA?\n");
+    printf("-------------------------------------------------------\n");
+    printf("Cada intento incorrecto reduce 1.\n");
+    printf("_______________________________________________________\n");
+    printf("4. ¿QUE PASA CUANDO FALLAS?\n");
+    printf("-------------------------------------------------------\n");
+    printf("Se te dara una pista.\n");
+    printf("_______________________________________________________\n");
+    printf("5. ¿CUANDO SE GANA?\n");
+    printf("-------------------------------------------------------\n");
+    printf("Adivina antes de quedarte sin vidas.\n");
+    printf("_______________________________________________________\n");
+    
+}
+
+int validarEntrada(int x){
+
+
+    if (x < 1 || x>99999){
+
+        x = 0;
+    }
+    else{
+        x = 1;
+    }
+
+    return x;
 
 }
